3cf0bcb6caad87b16c631ed0b298aae081d73c08
Xcpyk <snowride@qq.com>
2025-07-09 18:27:07 +0800

    更新新闻20250709

--- custom_news.py
+++ custom_news.py
@@ -14,31 +14,31 @@
 custom_news = [
     # 在这里添加你的自定义新闻
     {
-        'translated_title': '国内航线燃油附加费7月5日起上调10元',
+        'translated_title': '南京红老头',
         'original_title': 'Domestic air fuel surcharge to rise by 10 yuan from July 5',
-        'original_url': 'https://www.jiemian.com/article/12974758.html',
+        'original_url': 'https://mp.weixin.qq.com/s/GzfbO0wSRrlm4dfRVv7fFQ',
         'score': 1,
-        'time': 1751471304,  # 可选，Unix时间戳
+        'time': 1751943893,  # 可选，Unix时间戳
     },
     {
-        'translated_title': '两部门印发《支持创新药高质量发展的若干措施》',
+        'translated_title': '李爱庆权色交易被判死缓',
         'original_title': 'Two departments issue the "Supporting the High-Quality Development of Innovative Drugs" measures',
-        'original_url': 'https://www.jiemian.com/article/12974006.html',
+        'original_url': 'https://mp.weixin.qq.com/s/2Ya_b6LMU21N386rY3ywxQ',
         'score': 1,
-        'time': 1751471304,  # 可选，Unix时间戳
+        'time': 1751413673,  # 可选，Unix时间戳
     },
     {
-        'translated_title': '明起国内汽、柴油价格每吨分别上涨235元和225元',
+        'translated_title': '那尔那茜学历造假',
         'original_title': 'The domestic gasoline and diesel prices will rise by 235 yuan and 225 yuan per ton respectively from July 5',
-        'original_url': 'https://www.jiemian.com/article/12977140.html',
+        'original_url': 'https://mp.weixin.qq.com/s/uUOIW7w8mOl0OXQh29jW7g',
         'score': 1,
-        'time': 1751471304,  # 可选，Unix时间戳
+        'time': 1750528020,  # 可选，Unix时间戳
     },
     {
-        'translated_title': '充电宝上高铁要符合哪些条件？国铁集团回应',
+        'translated_title': '2025年开发者调查报告',
         'original_title': 'What are the conditions for charging batteries on high-speed trains? The China Railway Group responds',
-        'original_url': 'https://www.jiemian.com/article/12977256.html',
+        'original_url': 'https://mp.weixin.qq.com/s/mAsLW8WGBshsFWy5zBz2uQ',
         'score': 1,
-        'time': 1751471304,  # 可选，Unix时间戳
+        'time': 1752053520,  # 可选，Unix时间戳
     }
 ] 
